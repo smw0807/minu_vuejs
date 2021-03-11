5946e5434c9bff6e15960524d2c3bd30bfbe0d1f
smw0807 <smw0807@gmail.com>
2021-03-11 18:47:35 +0900

    [vue-cli+jwt]->NuxtJS로 구현(테이블 컴포넌트로 처리하는거 적용해봄)

---
+++ nuxt_web/web/components/table1.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>board2</h1>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Year</th>
+          <th>Name</th>
+          <th>Director</th>
+          <th>poster</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in list" :key="data.id">
+          <td>{{data.id}}</td>
+          <td>{{data.year}}</td>
+          <td>{{data.name}}</td>
+          <td>{{data.director}}</td>
+          <td><img :src="data.poster"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'table1',
+    props: ['list'],
+    
+}
+</script>
+
+<style>
+
+</style>
--- nuxt_web/web/pages/board2.vue
+++ nuxt_web/web/pages/board2.vue
@@ -1,46 +1,39 @@
 <template>
   <div>
-    <h1>board2</h1>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>Year</th>
-          <th>Name</th>
-          <th>Director</th>
-          <th>poster</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="data in list" :key="data.id">
-          <td>{{data.id}}</td>
-          <td>{{data.year}}</td>
-          <td>{{data.name}}</td>
-          <td>{{data.director}}</td>
-          <td><img :src="data.poster"></td>
-        </tr>
-      </tbody>
-    </table>
+    <button @click="test">test!</button>
+    <table-com :list="list" ></table-com>
   </div>
 </template>
 
 <script>
+const test = [
+  {"id":1,"name":"공조111111","year":2017,"director":"김성훈","poster":"http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79416/79416_185.jpg"},
+  {"id":2,"name":"컨택트22222","year":2017,"director":"드니 빌뇌브","poster":"http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79437/79437_185.jpg"},
+  {"id":3,"name":"더킹33333","year":2017,"director":"한재림","poster":"http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79423/79423_185.jpg"},
+]
+import table1 from '../components/table1';
 export default {
-    data () {
-      return {
-        list: []
-      }
+    // data () {
+    //   return {
+    //     list: []
+    //   }
+    // },
+    components: {
+      'table-com': table1
     },
     created() {
-      this.$axios.get('/v1/movies').then(res => {
-        if (res.status == 200) {
-          this.list = res.data;
-        }
-      })
-      .catch(err => {
-        console.log('board list error : ', err);
-      })
-    }
+      this.$store.dispatch('board/board/initBoardList');
+    },
+    computed: {
+      list() {
+        return this.$store.getters['board/board/boardList'];
+      }
+    },
+    methods: {
+      test() {
+        this.$store.commit('board/board/SET_BOARD_LIST', test);
+      }
+    },
 }
 </script>
 
