3939664e60c7b02fac9f53d4c6d452dd603dd23c
smw0807 <smw0807@gmail.com>
2021-03-28 21:13:05 +0900

    NuxtJS - vuetify 메뉴로 변경함

--- NuxtJS/nuxtjs_web/components/topMenu.vue
+++ NuxtJS/nuxtjs_web/components/topMenu.vue
@@ -8,25 +8,24 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-menu offset-y v-for="(menu, idx) in menus" :key="idx">
-        <template v-slot:activator="{ on }">
-          <v-btn text v-on="on">
-            <span>{{menu.name}}</span>
+      <v-menu offset-y open-on-hover v-for="(menu, idx) in menus" :key="idx">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-on="on" v-bind="attrs">
+            <span>{{ menu.name }}</span>
           </v-btn>
         </template>
 
         <v-list v-if="menu.child">
           <v-list-item v-for="(child, idx2) in menu.sub" :key="idx2">
             <v-list-item-title>
-              {{child.name}}
-              <!-- <nuxt-link :to="{path: sub.path}">{{ sub.name }}</nuxt-link> -->
+              <nuxt-link class="btn" :to="{path: child.path}">{{ child.name }}</nuxt-link>
             </v-list-item-title>
           </v-list-item>
         </v-list>
         <v-list v-else>
           <v-list-item>
             <v-list-item-title>
-              <nuxt-link :to="{path: menu.path}">{{ menu.name }}</nuxt-link>
+              <nuxt-link class="btn" :to="{path: menu.path}">{{ menu.name }}</nuxt-link>
             </v-list-item-title>
           </v-list-item>
         </v-list>
@@ -51,44 +50,30 @@ export default {
         },
         {
           name: '게시판',
-          path: '/board/board',
+          path: '/board/',
           child: true,
-          sub: {
-            name: '게시판1',
-            path: '/board/board'
-          }
-        },
-        {
-          name: 'test',
-          path: '/main',
-          child: false
+          sub: [
+            { name: '게시판1', path: '/board/board' }
+          ]
         },
         {
           name: 'Vuetify',
           path: '/vuetify/vuetify1',
           child: true,
-          sub: {
-            name: 'vuetify1',
-            path: '/vuetify/vuetify1'
-          }
+          sub: [
+            { name: 'vuetify1', path: '/vuetify/vuetify1' },
+            { name: 'vuetify2', path: '/vuetify/vuetify2' }
+          ]
         },
         {
           name: 'API Test',
           path: '/api/test',
           child: true,
-          sub: {
-            name: 'API 1',
-            path: '/api/test'
-          }
+          sub: [
+            { name: 'API 1', path: '/api/test'}
+          ]
         }
-      ],
-      menus1: [
-        {no: 0, path: '/main', name: 'Home'},
-        {no: 1, path: '/board/board', name: '게시판1'},
-        {no: 2, path: '/vuetify/vuetify1', name: 'Vuetify'},
-        {no: 3, path: '/api/test', name: 'API Test'}
-      ],
-
+      ]
     }
   },
   created() {
