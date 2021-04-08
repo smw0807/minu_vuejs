db3a666648334557b22593a0754c625d6ef5a3a2
smw0807 <smw0807@gmail.com>
2021-04-08 18:07:34 +0900

    NuxtJS > vuetify 디자인 작업

--- NuxtJS/nuxtjs_web/pages/setting/user.vue
+++ NuxtJS/nuxtjs_web/pages/setting/user.vue
@@ -1,10 +1,112 @@
 <template>
-  
+  <v-container fluid class="grey lighten-5 mb-6">
+      <v-row 
+        align="baseline"
+        justify="start"
+        >
+        <v-col cols="12">
+          <v-row></v-row>
+            <v-card outline>
+              <list-table :list="list"></list-table>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+  </v-container>
 </template>
 
 <script>
+import listTable from '~/components/setting/user/listTable'
 export default {
-
+  components: {
+    listTable
+  },
+  data() {
+    return {
+      list: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        }
+      ]
+    }
+  }
 }
 </script>
 
