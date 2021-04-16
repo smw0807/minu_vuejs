36228d2c28065033fa10b6ff72a6d05c846da16c
smw0807 <smw0807@gmail.com>
2021-04-17 00:27:10 +0900

    nuxt > vuetify에서 제공하는 input rules 기능 적용해봄

--- nuxt/web/components/setting/user/editUser.vue
+++ nuxt/web/components/setting/user/editUser.vue
@@ -16,33 +16,35 @@
           <span class="headline">사용자 등록</span>
         </v-card-title>
         <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field v-model="user_id" label="아이디*" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="user_nm" label="이름*" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="user_pw" label="비밀번호*" type="password" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="user_pw_chk" label="비밀번호 확인*" type="password" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-select
-              v-model="user_auth"
-              label="권한*"
-              :items="authList"
-              item-text="name"
-              item-value="value"
-              >
-              </v-select>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="user_desc" label="설명"></v-text-field>
-            </v-col>
-          </v-row>
+          <v-form ref="form">
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="user_id" label="아이디*" :rules="user_id_rule" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="user_nm" label="이름*" :rules="user_nm_rule" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="user_pw" label="비밀번호*" type="password" :rules="user_pw_rule" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="user_pw_chk" label="비밀번호 확인*" type="password" :rules="user_pw_rule" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                v-model="user_auth"
+                label="권한*"
+                :items="authList"
+                item-text="name"
+                item-value="value"
+                >
+                </v-select>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="user_desc" label="설명" :rules="user_desc_rule"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
           <small>*표시는 반드시 입력해야하는 항목입니다.</small>
         </v-card-text>
         <v-card-actions>
@@ -69,12 +71,40 @@ export default {
         { name: '일반 사용자', value: 'M'}
       ],
       user_id: '',
+      user_id_rule: [
+        v => !!v || '아이디는 필수 입력사항입니다.',
+        v => /^[a-zA-Z0-9]*$/.test(v) || '아이디는 영문+숫자만 입력 가능합니다.',
+        v => v.length <= 15 || '아이디는 15자 이상 입력할 수 없습니다.'
+      ],
       user_nm: '',
+      user_nm_rule: [
+        v => !!v || '이름은 필수 입력사항입니다.',
+        v => v.length <= 30 || '이름은 30자 이상 입력할 수 없습니다.',
+        v => !/[~!@#$%^&*()_+|<>?:{}]/.test(v) || '이름에는 특수문자를 사용할 수 없습니다.'
+      ],
       user_pw: '',
       user_pw_chk: '',
+      user_pw_rule: [
+        v => !!v || '패스워드는 필수 입력사항입니다.',
+        v => v.length <= 30 || '패스워드는 30자 이상 입력할 수 없습니다.'
+      ],
       user_auth: '',
-      user_desc: ''
+      user_desc: '',
+      user_desc_rule: [
+        v => v.length <= 100 || '설명은 100자 이상 입력할 수 업습니다.'
+      ]
     }
+  },
+  computed: {
+    nameErrors() {
+      const errors = [];
+      !this.$v.name.required && errors.push('Name is required.')
+      return error;
+    }
+  },
+  watch: {
+  },
+  created() {
   },
   methods: {
     save() {
