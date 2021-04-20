e790b008e9313c415f207ad6379e4573ecda9643
smw0807 <smw0807@gmail.com>
2021-04-20 21:29:22 +0900

    nuxt > 페이지 추가 (작업은 추후에)

---
+++ nuxt/web/components/setting/app/listTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card raised outlined class="pa-3" min-height="700">
+    <v-card-title>
+      <v-spacer></v-spacer>
+    </v-card-title>
+    <v-card-title></v-card-title>
+      a
+    </v-card-title>
+    <v-card-text>
+      z
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
