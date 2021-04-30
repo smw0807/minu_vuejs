5326adb94166d244879333719ea2143d4ed80f10
smw0807 <smw0807@gmail.com>
2021-04-30 11:57:34 +0900

    nuxt > 오버레이 로딩창 색 추가 다시...

--- nuxt/web/components/cmn/overlay.vue
+++ nuxt/web/components/cmn/overlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-overlay :value="overlay" z-index="10000"></v-overlay>
+  <v-overlay :value="overlay" z-index="10000">
     <v-progress-circular indeterminate size="64" color="primary"></v-progress-circular>
   </v-overlay>
 </template>
