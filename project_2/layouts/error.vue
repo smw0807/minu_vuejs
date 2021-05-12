aa97dd8863f5477cb70a1c780bdb1d8538927193
smw0807 <smw0807@gmail.com>
2021-05-12 10:24:45 +0900

    project_1, project_2 > 에러 페이지 수정

--- project_2/layouts/error.vue
+++ project_2/layouts/error.vue
@@ -1,14 +1,30 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-row align="center" justify="center">
+      <v-col cols="6">
+        <v-alert
+          border="right"
+          colored-border
+          type="error"
+          elevation="2"
+        >
+          <template v-if="error.statusCode === 404">
+            <h1> {{ pageNotFound }} </h1>
+          </template>
+          <template v-else>
+            <h1> {{ otherError }} </h1>
+            <p> {{msg}}</p>
+          </template>
+          <v-btn
+            link
+            raised outlined
+            to="/"
+            >
+            <v-icon>mdi-home-variant-outline</v-icon> 메인으로 돌아가기
+          </v-btn>
+        </v-alert>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
@@ -23,13 +39,17 @@ export default {
   },
   data () {
     return {
+      msg: '',
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred'
     }
   },
   head () {
+    console.log(this.error);
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+
+    this.msg = this.error.message;
     return {
       title
     }
