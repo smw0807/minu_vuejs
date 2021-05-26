9a51a1554573a000ad0a0d72b3bac9e2cf15f997
smw0807 <smw0807@gmail.com>
2021-05-26 23:04:51 +0900

    nuxt_jquery > nuxtjs에 jquery 적용 확인

---
+++ nuxt_jquery/web/pages/jquery.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout column>
+    <v-flex>
+      <v-card>
+        <v-card-title>jQUeryJS</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="9">
+              <v-text-field id="text"></v-text-field>
+            </v-col>
+            <v-col cols="3">
+              <v-btn id="btn" @click="btn">Check</v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  methods: {
+    btn() {
+      console.log('btn!!');
+      var text = $('#text').val();
+      console.log(text);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
