3522581e56aed0511ca168fdb52ff9171e2f4952
smw0807 <smw0807@gmail.com>
2021-05-27 19:33:54 +0900

    project_1 > vue-slimgrid 테스트 중

---
+++ project_1/web/components/slimgrid/grid1.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <slim-grid :column-options="columnOptions" :data="rows"></slim-grid>
+  </div>
+</template>
+
+<script>
+export default { 
+  data() {
+    return {
+      rows: [
+        { id: 1, name: 1 , value: 'a' },
+        { id: 2, name: 2 , value: 'a' },
+        { id: 3, name: 3 , value: 'a' },
+        { id: 4, name: 4 , value: 'a' },
+        { id: 5, name: 5 , value: 'a' },
+        { id: 6, name: 6 , value: 'a' },
+      ],
+      columns: [
+        { id: 'name', field: 'name', name:'이름'},
+        { id: 'value', field: 'value', name:'값'}
+      ],
+      columnOptions: {
+        'name':{
+          id: 'name',
+          name: 'Name',
+          headerInput: false,
+          headerFilter: false,
+          formatter(row, cell, value) {
+            return value
+          }
+        },
+        'value':{
+          id:'value',
+          name: 'Value',
+          headerInput: false,
+          headerFilter: false,
+          formatter(row, cell, value) {
+            return value
+          }
+        }
+      },
+    }
+  }
+}
+</script>
+
+<style>
+.disabled {
+  height: 95%;
+  display: block;
+  padding: 0px;
+  white-space: nowrap;
+  text-align: center;
+  background-color: #8795A1;
+  color: #ffffff;
+}
+</style>
---
+++ project_1/web/pages/slimgrid/grid1.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout column>
+    <v-flex class="mt-4 ml-9">
+      <v-row>
+        <v-col cols="12" align="start">
+          <h2>vue-slimgrid</h2>
+        </v-col>
+      </v-row>
+    </v-flex>
+
+    <v-flex class="ma-4">
+      <v-row class="px-4">
+        <v-col cols="6">
+          
+        </v-col>
+      </v-row>
+
+      <v-row class="px-4">
+        <v-col>
+          
+        </v-col>
+      </v-row>
+    </v-flex>
+    
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
