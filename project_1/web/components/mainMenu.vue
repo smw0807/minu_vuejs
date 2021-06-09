80a63afeb43f75670f5c24ff78f36d4a9d574e6f
smw0807 <smw0807@gmail.com>
2021-06-09 15:07:08 +0900

    project_1 > 메뉴 구조 바꿔보기(준비단계만 커밋)

--- project_1/web/components/mainMenu.vue
+++ project_1/web/components/mainMenu.vue
@@ -1,5 +1,84 @@
 <template>
   <div>
+    <!-- <v-app-bar
+        app
+        dark
+      >
+      <v-app-bar-nav-icon @click.stop="drawer =!drawer"></v-app-bar-nav-icon>
+      <v-divider vertical class="ml-1"></v-divider>
+      <v-breadcrumbs
+        :items="test"
+        large
+      ></v-breadcrumbs>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      id="main_nav"
+      app
+      v-model="drawer"
+      :clipped="clipped"
+      :expand-on-hover="drawer"
+      permanent
+      dark
+      >
+      <v-list dense nav>
+
+        <v-list-item :class="miniVariant && 'px-0 ma-0'">
+          <v-list-item-avatar>
+            <img src="/v.png"  style="width: 30px; height: 30px; margin: 0;">
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title >AF Manager</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider dark></v-divider> 
+        
+        <template v-for="item in items">
+          <v-list-group
+            v-if="item.items"
+            :key="item.title"
+            :prepend-icon="item.action"
+            class="mb-5"
+            >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item
+              v-for="subItem in item.items"
+              :key="subItem.title"
+              :to="subItem.to"
+              class="ml-3"
+              >
+              <v-list-item-icon>
+                <v-icon small v-if="subItem.icon">{{subItem.icon}}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="subItem.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+
+          <v-list-item 
+            v-else
+            :to="item.to"
+            :key="item.title"
+            class="mb-5"
+            >
+            <v-list-item-icon>
+                <v-icon v-if="item.action">{{item.action}}</v-icon>
+              </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+     -->
     <v-navigation-drawer 
       v-model="drawer"
       app
@@ -8,26 +87,26 @@
       permanent
       >
       <v-list dense>
-        <template v-for="menu in menus">
+        <template v-for="item in items">
           <!-- 구분선 긋기 -->
           <v-list-item
-            v-if="menu.divider"
-            :key="menu.title"
+            v-if="item.divider"
+            :key="item.title"
             >
             <v-divider dark></v-divider>
           </v-list-item>
 
           <!-- 메뉴 넣기 -->
           <v-list-item 
-            :to="menu.to"
-            :key="menu.title"
+            :to="item.to"
+            :key="item.title"
             v-else
             >
             <v-list-item-icon>
-              <v-icon>{{ menu.icon }}</v-icon>
+              <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title>{{ menu.title }}</v-list-item-title>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
@@ -61,7 +140,7 @@ export default {
       org: false, 
       drawer: true,
       clipped: true,
-      menus: [
+      items: [
         { title: 'index', icon: 'mdi-view-dashboard', to: '/' },
         { title: 'test', icon: 'mdi-api', to: '/api/apiTest' },
         { divider: true },
@@ -75,6 +154,35 @@ export default {
         { title: 'v-picker', icon: 'mdi-calendar', to: '/vpicker/datepicker'},
         { divider: true },
         { title: 'vue slimgrid', icon: 'mdi-grid', to: '/slimgrid/grid1'},
+      ],
+      temp: [
+        {
+          action: 'mdi-view-dashboard',
+          title: 'menu1',
+          to:'/dashboard'
+        },
+        {
+          action: 'mdi-alert-outline',
+          title: 'menu2',
+          active: true,
+          items: [
+            { title: 'sub1', to: '/threat/monitoring', icon: 'mdi-monitor-dashboard' },
+            { title: 'sub2', to: '/threat/analysis', icon: 'mdi-playlist-check' },
+            { title: 'sub3', to: '/threat/threat', icon:'mdi-sync-alert' },
+            { title: 'sub4', to: '/threat/threat', icon:'mdi-radar' },
+            { title: 'sub5', to: '/threat/threat', icon:'mdi-magnify' }
+          ],
+        },
+        {
+          action: 'mdi-traffic-light',
+          title: 'menu3',
+          items: [
+            { title: 'sub1', to: '/traffic/allTraffic', icon: 'mdi-traffic-light' },
+            { title: 'sub2', to: '/traffic/all', icon: 'mdi-account-multiple' },
+            { title: 'sub3', to: '/traffic/all', icon:'mdi-magnify' },
+            { title: 'sub4', to: '/traffic/serverPkt', icon:'mdi-text-box-search' }
+          ],
+        },
       ]
     }
   },
