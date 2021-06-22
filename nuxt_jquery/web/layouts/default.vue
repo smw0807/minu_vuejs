656b23dbe48a38f709647eb814273b68b095ad97
smw0807 <smw0807@gmail.com>
2021-06-22 20:23:11 +0900

    nuxt_jquery > 확인해보고 싶은게 있어서 테마 변경 컴포넌트 넣음

--- nuxt_jquery/web/layouts/default.vue
+++ nuxt_jquery/web/layouts/default.vue
@@ -52,12 +52,8 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      
+      <change-theme/>
     </v-app-bar>
     <alert/>
     <v-main>
