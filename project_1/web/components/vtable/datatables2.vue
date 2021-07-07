a4f99cad37188862a32dbf4b9d1c78da2e8aa03e
smw0807 <smw0807@gmail.com>
2021-07-07 21:45:48 +0900

    project_1 > vuetify datatables tr, td 태그 접근과 수정해보기 datatables 안에 클릭한 버튼 위치의 tr 정보 접근과 그 안의 각 td 접근 방법은 성공했다. 이제 각 td 안에 text를 v-text-field로 바꾸고 버튼도 다른 걸로 변경하는게 다음 목표 이후 다시 원복하거나 입력한 값을 저장하는 로직까지 하는게 최종 목표

--- project_1/web/components/vtable/datatables2.vue
+++ project_1/web/components/vtable/datatables2.vue
@@ -10,7 +10,13 @@
         label="Search"
         single-line
       ></v-text-field>
+    </v-card-title>
+    <v-card-title>
       <v-btn color="primary" @click="add">Add</v-btn>
+      <v-btn color="primary" class="ml-1" @click="add2">Add2</v-btn>
+      <v-btn class="ml-1" @click="add3">Add3</v-btn>
+      <v-btn class="ml-1" @click="jsx">JSX</v-btn>
+      <v-btn small class="ml-1"><v-icon>mdi-cancel</v-icon></v-btn>
     </v-card-title>
     <v-card-text>
       <v-data-table
@@ -24,23 +30,30 @@
         height="550"
         ref="table"
         >
-        <!-- <template v-slot:[`item.data1`]="{ item }">
-          <v-edit-dialog
-            :return-value.sync="item.data1"
-            lazy
-           
-          > {{item.data1}}
-            <template v-slot:[`input`]>
-              <v-text-field
-                v-model="item.data1"
-                label="Edit"
-                single-line
-                counter
-              ></v-text-field>
-            </template>
-          </v-edit-dialog>
-        </template> -->
-        <template v-slot:[`item.actions`]="{ index }">
+        <template v-slot:[`item`]="{ item, index}">
+          <tr :class="'item_' + index">
+            <td>{{ item.data1 }}</td>
+            <td>{{ item.data2 }}</td>
+            <td>{{ item.data3 }}</td>
+            <td>
+              <v-icon
+                small
+                class="mr-2 btn_action"
+                @click="edit(index, $event)"
+                style="color:#1976d2;">
+                mdi-magnify
+              </v-icon>
+              <v-icon
+                small
+                class="btn_action"
+                @click="del(index)"
+                style="color:#e23d3d;">
+                mdi-delete
+              </v-icon>
+            </td>
+          </tr>
+        </template>
+        <!-- <template v-slot:[`item.actions`]="{ index }">
           <v-icon
             small
             class="mr-2 btn_action"
@@ -48,7 +61,6 @@
             style="color:#1976d2;">
             mdi-magnify
           </v-icon>
-            <!-- @click="edit(item)" -->
           <v-icon
             small
             class="btn_action"
@@ -56,17 +68,33 @@
             style="color:#e23d3d;">
             mdi-delete
           </v-icon>
-        </template>
+        </template> -->
       </v-data-table>
       <div class="text-center">
         <v-pagination v-model="page" :length="pageCount"></v-pagination>
+      </div>
+      <div id="test">
+        <jsx/>
       </div>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+const jsx_test = {
+  render(h) {
+    return (
+      <div>
+        <v-btn small class="ml-1"><v-icon>mdi-check</v-icon></v-btn>
+        <v-btn small class="ml-1"><v-icon>mdi-close</v-icon></v-btn>
+      </div>
+    )
+  },
+}
 export default {
+  components:{
+    'jsx' : jsx_test
+  },
   data() {
     return {
       search: '',
@@ -96,13 +124,45 @@ export default {
       const info = this.$refs.table;
 
     },
+    add2() {
+      const icon = document.createElement('i');
+      icon.className = "mdi mdi-cancel";
+      // icon.innerHTML = "folder_open";
+      var main = document.getElementById('test');
+      main.appendChild(icon);
+    },
+    add3() {
+      const btn1 = document.createElement('button');
+      btn1.className = 'v-btn v-btn--is-elevated v-btn--has-bg theme--dark v-size--default primary small';
+      const icon = document.createElement('i');
+      icon.className = "mdi mdi-chceck";
+      btn1.innerHTML = '?';
+      var main = document.getElementById('test');
+      main.appendChild(btn1);
+    },
+    jsx() {
+      console.log(jsx_test);
+      // var main = document.getElementById('test');
+      // main.appendChild(jsx_test);
+    },
     edit(v, e) {
       console.log('edit', v, e);
-      const info = this.$refs.table;
-      console.log(info);
-      // const test = document.getElementsByTagName('>td', v);
+      const test = document.getElementsByClassName('item_' + v);
       console.log(test);
-      
+      const el = test[0];
+      console.log(el.cells[0]);
+      console.log(el.cells[1]);
+      console.log(el.cells[2]);
+      console.log(el.cells[3]);
+      el.cells[3].innerHTML = '<v-icon>mdi-check</v-icon>'
+      /**
+       * 2021-07-07
+       * 여기서 문제,
+       * 각 tr 태그 안에 innerHTML로 데이터를 넣으면 vuetify 컴포넌트 적용이 안된다.
+       * 화면상에 실제 그려지는건 vuetify 태그가 아니기 때문에...
+       * 위에 add3 함수 처럼 createElement를 이용해서 만들어야 할 것 같은데 
+       * 조금만 더 하면 만들 수 있을 것 같다.
+       */
     },
     del(v) {
       console.log('del', v);
