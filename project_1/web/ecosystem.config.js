0095176108ee62bfdfef7af2a1ded789d47a33da
smw0807 <smw0807@gmail.com>
2021-07-09 17:16:23 +0900

    project_1 > PM2 테스트 해보기

---
+++ project_1/web/ecosystem.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  apps: [
+    {
+      name: 'AF_Manage',
+      // package.json에 정의된 npm run start를 실행하게 해서 PM2로 관리하게 한다.
+      port: 3000,
+      exec_mode: 'cluster',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      // script: '/root/af_mng/node_modules/nuxt/bin/nuxt.js',
+      // script: 'npm',
+      // cwd: '/root/af_mng/',
+      // args: 'dev',
+      args: 'start',
+      instances: 1,
+      // autorestart: true,
+      // watch: false,
+      // max_memory_restart: '2G',
+      // 개발 환경에서 적용될 설정
+      // pm2 start ecosystem.config.js
+      // pm2 start ecosystem.config.js --env production
+      // env_production: {
+      //   HOST: '0.0.0.0',
+      //   PORT: 3000,
+      //   NODE_ENV: 'production'
+      // },
+      // 배포 환경에서 적용될 설정
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+          NODE_ENV: 'production',
+      },
+      output: './logs/console.log',
+      error: './logs/consoleError.log'
+    }
+  ]
+  // deploy: {
+  //   production: {
+  //     // sample
+  //     user: 'node',
+  //     host: 'localhost',
+  //     ref: 'origin/master',
+  //     // repo: 'git@github.com:repo.git',
+  //     path: '/root/nuxt/web',
+  //     'post-deploy':
+  //       'npm install && pm2 reload ecosystem.config.js --env production'
+  //   }
+  // }
+};
