f061d31d5ad339e8e7d28e95e20b7e0818cda9a6
smw0807 <smw0807@gmail.com>
2021-07-20 14:14:21 +0900

    project_1 > global filters

--- project_1/web/nuxt.config.js
+++ project_1/web/nuxt.config.js
@@ -34,6 +34,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/axios.js',
+    '@/plugins/filters.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ project_1/web/plugins/filters.js
@@ -0,0 +1,9 @@
+/**
+ * 글로벌 필터 기능 넣는곳
+ */
+import Vue from 'vue';
+
+//숫자에 콤마 넣어주는거 
+Vue.filter('ft_makeComma', val => {
+  return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
