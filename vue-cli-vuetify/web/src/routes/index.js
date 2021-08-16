a590d0d2a1b7fdfc5f9052f7387d494ef15a5cf6
smw0807 <smw0807@gmail.com>
2021-08-16 16:22:22 +0900

    vue-cli-vuetify > vue-router 적용 하나하나 차근차근 하려고했는데 라우터 부터 잘 안된다. 뭐가 문제인지를 모르겠다. nuxt에 너무 익숙해진듯

--- vue-cli-vuetify/web/src/routes/index.js
+++ vue-cli-vuetify/web/src/routes/index.js
@@ -1,1 +1,23 @@
 //route dir
+//vue-router 설치해야함!!!
+import Vue from 'vue';
+import VueRouter from 'vue-router'
+
+import main from '@/views/index'
+
+Vue.use(VueRouter);
+
+//라우터 정보 입력
+const routes = [
+  {
+    path: '/test',
+    name: 'main',
+    component: main
+  }
+]
+console.log('111');
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+export default router
---
+++ vue-cli-vuetify/web/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout column>
+    <v-row>
+      <v-col>
+        test
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name : 'main',
+  created() {
+    console.log('???');
+  }
+}
+</script>
+
+<style>
+
+</style>
