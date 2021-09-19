9ef2f91fe261c22055326aeda6cbb7cf1b49d125
smw0807 <smw0807@gmail.com>
2021-09-19 18:51:01 +0900

    project_1 > resize directive 만들어보기 mixin으로 만들었던 height resize 기능을 디렉티브로 만들어 봤는데 소스가 좀 아쉬움...더 잘 만들 수 있을 것 같은데 방법디 안 떠오른다.

---
+++ project_1/web/components/vue/directive/directive2.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card outlined>
+    <v-card-title>mixin으로 만든 resize 기능 directive로 만들어보기</v-card-title>
+    <v-card-text>
+      <v-card v-auto-height="500">
+        <v-card-text>
+          ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+        </v-card-text>
+      </v-card>
+    </v-card-text>
+    <!-- <v-card-text>
+      <v-card v-auto-height="200">
+        <v-card-text>
+          ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
+        </v-card-text>
+      </v-card>
+    </v-card-text> -->
+  </v-card>
+</template>
+
+<script>
+export default {
+  directives: {
+    'auto-height': {
+      bind(el, binding) {
+        //처음 높이 셋팅
+        let w_height = window.innerHeight;
+        if (binding.expression) {
+          w_height =  w_height - Number(binding.expression);
+        }
+        el.style.height = w_height + 'px';
+
+        //resize로 높이 변경 이벤트 발생하는 곳
+        window.addEventListener('resize', function (e) {
+          let t_height = e.target.innerHeight;
+          if (binding.expression) {
+            t_height = t_height - Number(binding.expression);
+          }
+          el.style.height = t_height + 'px';
+        });
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- project_1/web/pages/vue/vue1.vue
+++ project_1/web/pages/vue/vue1.vue
@@ -6,15 +6,22 @@
           <directive1/>
         </v-col>
       </v-row>
+      <v-row>
+        <v-col cols="12">
+          <directive2/>
+        </v-col>
+      </v-row>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 import directive1 from '@/components/vue/directive/directive1'
+import directive2 from '@/components/vue/directive/directive2'
 export default {
   components: {
-    directive1
+    directive1,
+    directive2
   }
 }
 </script>
