637d0bfb3acd9ce478ded9c43ef47846051da6bc
smw0807 <smw0807@gmail.com>
2021-09-23 21:31:48 +0900

    vue-cli-vuetify > 레이아웃 작업 및 테마 변경 vuerouter 관련 에러가 뜨는게 있는데 이유를 모르겠음

--- vue-cli-vuetify/web/src/App.vue
+++ vue-cli-vuetify/web/src/App.vue
@@ -1,23 +1,90 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <!-- <v-app-bar app >
       <div class="d-flex align-center">
         JWT Login
       </div>
-
       <v-spacer></v-spacer>
+    </v-app-bar> -->
+    <v-app-bar
+      app
+      flat
+    >
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
 
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="grey darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
     </v-app-bar>
 
-    <v-main>
+    <v-main >
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!-- 왼쪽 -->
+            test
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!-- 센터  -->
+              <router-view/>
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!-- 오른쪽 -->
+              ㅋㅋ
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <!-- <v-main>
       <v-container>
         <router-view/>
       </v-container>
-    </v-main>
+    </v-main> -->
   </v-app>
 </template>
 
@@ -25,15 +92,22 @@
 
 export default {
   name: 'App',
-
+  data() {
+    return {
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }
+  },
   components: {
   },
 
   cretaed() {
-    console.log('222');
   },
   mounted() {
-    console.log('333');
   },
 
 };
