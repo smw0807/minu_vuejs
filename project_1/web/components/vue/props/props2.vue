d81632313a92823ba61749bf72ebf0ae09fba909
smw0807 <smw0807@gmail.com>
2021-12-04 18:13:46 +0900

    project_1 > props 테스트

--- project_1/web/components/vue/props/props2.vue
+++ project_1/web/components/vue/props/props2.vue
@@ -4,8 +4,11 @@
     <v-card-text>
       <v-data-table
         :headers="headers"
-        :items="data2"
+        :items="dataChange"
       ></v-data-table>
+    </v-card-text>
+    <v-card-text>
+      {{test3}}
     </v-card-text>
   </v-card>
 </template>
@@ -24,17 +27,24 @@ export default {
         { text: 'sensor_id', value: 'sensor_id'},
         { text: 'in_byte', value: 'in_byte'},
         { text: 'out_byte', value: 'out_byte'},
-      ]
+      ],
+      test3: {
+        type: Object,
+        default: null
+      }
     }
   },
   computed: { //전달된 props를 바꾸어야할 경우 computed를 사용하는 것이 좋음!!!!!
-    data2() {
+    dataChange() {
       return this.test(this.data);
     }
   },
   methods: {
     test(v) {
       console.log('test : ', v);
+      const d = v[0];
+      console.log(d);
+      this.test3 = d;
       for (let item of v) {
         if(item.sensor_id) {
           item.sensor_id = item.sensor_id + '번 장비'
