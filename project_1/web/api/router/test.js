d2560d0a1909ecaa5a4b33be257e65927556bf11
smw0807 <smw0807@gmail.com>
2021-12-06 21:24:02 +0900

    project_1> worker_thread를 이용해 엘라스틱 데이터 csv 만들기 엘라스틱에 진행상황 업데이트하게 수정..

--- project_1/web/api/router/test.js
+++ project_1/web/api/router/test.js
@@ -33,8 +33,9 @@ router.post('/worker_test', async (req, res) => {
   let rt = {};
   try {
     //파일 생성하는 워커 돌림
-      const query = { "query":{"bool":{"filter":[{"range":{"create_date":{"gte":"2021-11-17 15:34:01","lte":"2021-11-17 15:34:59"}}}],"must":[],"must_not":[]}}};
-      // const query = { "query":{"bool":{"filter":[{"range":{"create_date":{"gte":"2021-11-15 00:00:01","lte":"2021-11-17 23:34:59"}}}],"must":[],"must_not":[]}}};
+      // const query = { "query":{"bool":{"filter":[{"range":{"create_date":{"gte":"2021-11-17 11:34:01","lte":"2021-11-17 15:34:59"}}}],"must":[],"must_not":[]}}};
+      // const query = { "query":{"bool":{"filter":[{"range":{"create_date":{"gte":"2021-11-17 15:34:01","lte":"2021-11-17 15:34:59"}}}],"must":[],"must_not":[]}}};
+      const query = { "query":{"bool":{"filter":[{"range":{"create_date":{"gte":"2021-11-15 00:00:01","lte":"2021-11-17 23:34:59"}}}],"must":[],"must_not":[]}}};
       const index_name = 'ni_raw_threat-20211115,ni_raw_threat-20211116,ni_raw_threat-20211117';
       const headers = {"create_date":"수집일시","date_time":"탐지일시","sensor_id":"수집장비","type":"구분","attack_ip":"공격IP","attack_port":"공격포트","attack_iso":"공격국가","attack_loc":"공격지","victim_ip":"피해IP","victim_port":"피해포트","victim_iso":"피해국가","victim_loc":"피해지","inst_type":"등록기관","intent":"공격의도","n_org":"그룹명","risk":"위험도","rule_cate_m":"주분류","rule_cate_s":"세분류","rule_name":"탐지규칙명","proto":"프로토콜","payload":"페이로드","http_info":"도메인","detec_file_hash":"해쉬","detec_file_name":"파일명"};
       // const fields = ["create_date","date_time","sensor_id","type","attack_ip","attack_port","attack_iso","attack_loc","victim_ip","victim_port","victim_iso","victim_loc","inst_type","intent","n_org","risk","rule_cate_m","rule_cate_s","rule_name","proto","http_info","detec_file_hash","detec_file_name"];
--- project_1/web/api/worker/makeFile.js
+++ project_1/web/api/worker/makeFile.js
@@ -8,45 +8,110 @@ const elasticsearch = require('elasticsearch');
 const es_client = new elasticsearch.Client({
   hosts: String(process.env.ES_HOST).split(',')
 })
-const index_name = 'ni_manage';
-const filePath = aRoot + '/static/file/';
+const index_name = 'ni_file_manage';
+const filePath = aRoot + '/static/file/'; //env로 관리?
 
 /**
  * file_name : 파일명
  * file_size : 파일 크기
  * file_mk_dt : 파일 생성일
- * file_state : 파일 처리 상태 (0: 생성중, 1: 생성완료, 2: 생성실패, 3: 생성 중지됨)
- * 0 : 다운로드 버튼 비활성화, 중지 버튼 활성화
- * 1 : 다운로드 버튼 활성화, 삭제 버튼 활성화
- * 2 : 다운로드 버튼 비활성화, 삭제 버튼 활성화
+ * file_state : 파일 처리 상태 (0: 생성 대기중, 1: 생성중, 2: 생성완료, 3: 생성실패, 4: 생성 중지됨)
+ * 0, 1 : 다운로드 버튼 비활성화, 중지 버튼 활성화
+ * 2 : 다운로드 버튼 활성화, 삭제 버튼 활성화
  * 3 : 다운로드 버튼 비활성화, 삭제 버튼 활성화
+ * 4 : 다운로드 버튼 비활성화, 삭제 버튼 활성화
  */
 
 //파일 정보 엘라스틱에 등록 (file_state : 0)
-async function insertInfo (v) {
-
+async function insertInfo (name, path) {
+  try {
+    const b = {
+      file_name: name,
+      file_size: 0,
+      file_path: path,
+      file_state: '0',
+      file_mk_dt : moment().format('YYYY-MM-DD HH:mm:ss')
+    }
+    const rs = await es_client.index({
+      index: index_name,
+      type: '_doc',
+      body: b
+    })
+    return true;
+  } catch (err) {
+    log.error(err);
+    console.error(err);
+    return false;
+  }
 }
 //엘라스틱에 파일 등록 여부 확인
-async function checkFile (v) {
-
+async function checkFile (name, state) {
+  try {
+    const query = {
+      query :{
+        bool: {
+          filter:[
+            {
+              term:{
+                file_name: name
+              }
+            },
+            {
+              term:{
+                file_state: state
+              }
+            }
+          ]
+        }
+      }
+    }
+    const rs = await es_client.count({
+      index: index_name,
+      type: '_doc',
+      body: query
+    })
+    if (rs.count === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (err) {
+    log.error(err);
+    console.error(err);
+    return false;
+  }
 }
 /** 엘라스틱에 등록된 파일 중간 업데이트
- * file_state : 0, file_size -> update
+ * 생성중 - file_state : 1, file_size update
+ * 성공시 - file_state : 2, file_size update
+ * 실패시 - file_state : 3, file_size update
  * @param {*} name 파일명
  * @param {*} size 파일 사이즈
- */
-async function updateRunningInfo (name, size) {
-
-}
-/** 엘라스틱에 등록된 파일 종료 업데이트
- * 성공시 - file_state : 1, file_size update
- * 실패시 - file_state : 2, file_size update
- * @param {*} name 파일명
  * @param {*} state 파일 처리 상태
- * @param {*} size 파일 사이즈
  */
-async function updateFinishInfo (name, state, size) {
-
+async function updateFileInfo (name, size, state) {
+  try {
+    const rs = await es_client.updateByQuery({
+      index: index_name,
+      type: '_doc',
+      refresh: true,
+      body: {
+        script: {
+          source: `ctx._source["file_state"]="${state}";ctx._source["file_size"]=${size}` 
+        },
+        query:{
+          term: {
+            file_name: name
+          }
+        }
+      }
+    })
+    return true;
+  } catch (err) {
+    log.error(err);
+    console.error(err);
+    return false;
+  }
 }
 
 //type 형식 데이터
@@ -99,7 +164,6 @@ async function run() {
     }
     const type = workerData.type; //데이터 형식 type 이냐 아니냐 여부, 맞으면 true 아니면 false
     const idx_name = workerData.idx_name;
-    // const file_name = workerData.file_name + '_' + new Date().getTime() + '.csv';
     const file_name = workerData.file_name.replace('.csv', '') + '_' + moment().format('YYYYMMDD-HHmmss') + '.csv';
     const headers = workerData.headers; // object, 표기할 필드 맵핑시킬 한글 정보 ex) { ip: '아이피', name: '이름' }
     const fields = workerData.fields; //array, 데이터 중에 csv에 표기할 필드 ex) ['ip', 'name']
@@ -110,47 +174,84 @@ async function run() {
     fields.forEach((doc, index) => {
       csv_header += (index != fields.length - 1 ? (headers[doc] + ',') : (headers[doc] + '\r\n'));
     });
-
+    
+    let state = true; //정상처리 여부
     //파일 생성
     const file = filePath + file_name;
     const writeStream = fs.createWriteStream(file);
     writeStream.on('finish', () => {
       const eTime = new Date().getTime();
+      //종료 업데이트
       console.log('파일 쓰기 완료', (eTime - sTime) / 1000 + 's');
     });
     writeStream.write('\uFEFF' + csv_header);
     
-    //검색
-    let rs = await es_client.search({
-      index: idx_name,
-      type: '_doc',
-      body: query,
-      scroll: '5m'
-    })
-
-    //스크롤 검색
-    while(rs.hits.hits.length > 0) {
-      let result = [];
-      //데이터 변환
-      if (type) {
-        result.push(TypeData(rs));
-      } else {
-        result.push(noTypeData(rs));
-      }
-      //변환된 데이터를 csv 형식 string으로 변환 작업
-      result.flat().forEach((doc) => {
-        let csv_string = '';
-        for (let item of fields) {
-          csv_string += doc[item] === undefined ? '' : data(doc[item]);
-          csv_string += ',';
-        }
-        csv_string += '\r\n';
-        writeStream.write('\uFEFF' + csv_string); //1개 row 마다 생성된 파일에 쓰기
-
+    //엘라스틱에 등록
+    const insert_es = await insertInfo(file_name, filePath);
+    //등록 여부 확인
+    const check_file = await checkFile(file_name, '0');
+    if (insert_es && check_file) {
+      //검색
+      let rs = await es_client.search({
+        index: idx_name,
+        type: '_doc',
+        body: query,
+        scroll: '5m'
       })
-      rs = await es_client.scroll({ scrollId: rs._scroll_id, scroll: '5m'});
+      let first = true;
+      
+      //스크롤 검색
+      while(rs.hits.hits.length > 0) {
+        if (!first) {
+          //첫 while문 사이클 이후 다시 돌기 전에 한번더 체크, 중간에 사용자가 생성 취소시 중단시키게
+          const check = await checkFile(file_name, '1');
+          if (!check) {
+            state = false;
+            let stat = fs.statSync(file);
+            const fail = await updateFileInfo(file_name, stat.size, "4");
+            break;
+          }
+        }
+        first = false;
+        let result = [];
+        //데이터 변환
+        if (type) {
+          result.push(TypeData(rs));
+        } else {
+          result.push(noTypeData(rs));
+        }
+        //변환된 데이터를 csv 형식 string으로 변환 작업
+        result.flat().forEach((doc) => {
+          let csv_string = '';
+          for (let item of fields) {
+            csv_string += doc[item] === undefined ? '' : data(doc[item]);
+            csv_string += ',';
+          }
+          csv_string += '\r\n';
+          writeStream.write('\uFEFF' + csv_string); //1개 row 마다 생성된 파일에 쓰기
+  
+        })
+        //중간 업데이트
+        let fileStat1 = fs.statSync(file);
+        const upd = await updateFileInfo(file_name, fileStat1.size, "1"); 
+        if (upd) { //중간 업데이트 성공시
+          rs = await es_client.scroll({ scrollId: rs._scroll_id, scroll: '5m'});
+        } else { //중간 업데이트 실패시
+          state = false;
+          const fail = await updateFileInfo(file_name, fileStat2.size, "3");
+          break;
+        }
+      }
+      writeStream.end(); //파일 쓰기 종료
+      if (state) {
+        let fileStat2 = fs.statSync(file);
+        let upd = await updateFileInfo(file_name, fileStat2.size, "2");
+      }
+    } else {
+      writeStream.end();
+      let fileStat3 = fs.statSync(file);
+      let upd = await updateFileInfo(file_name, fileStat3.size, "3");
     }
-    writeStream.end(); //파일 쓰기 종료
     parentPort.close(); //워커 종료
   } catch (err) {
     log.error(err);
