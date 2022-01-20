983a4fdeb644bf3e1b2c9a726c01b0562807a99c
smw0807 <smw0807@gmail.com>
2022-01-20 23:53:10 +0900

    project_1 > vue-modal-window 테스트 vuetify css랑 충돌나는건지 디자인도 깨지고, 드래그를 위해 클릭시 포지션도 꺠짐..

--- project_1/web/components/npm/modal/modal-window.vue
+++ project_1/web/components/npm/modal/modal-window.vue
@@ -1,23 +1,40 @@
+/**
+https://github.com/moritanian/vue-modal-window
+ */
 <template>
-  <v-card>
-    <v-card-title>vue-modal-window</v-card-title>
-    <v-card-text>
-      <v-layout column>
-        <v-row>
-          <v-col cols="2">
-            <v-btn @click="open">modal open</v-btn>
-          </v-col>
-        </v-row>
-      </v-layout>
-    </v-card-text>
-  </v-card>
+  <div>
+    <vue-modal-window :title="md_title" :draggable="true" :resizable="true" :visible.sync="open1"></vue-modal-window>
+    <v-card>
+      <v-card-title>vue-modal-window</v-card-title>
+      <v-card-text>
+        <v-layout column>
+          <v-row>
+            <v-col cols="2">
+              <v-btn @click="open">modal open</v-btn>
+            </v-col>
+          </v-row>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import VueModalWindow from 'vue-modal-window';
 export default {
+  components: {
+    VueModalWindow
+  },
+  data() {
+    return {
+      open1: false,
+      md_title: 'modal title?'
+    }
+  },
   methods:{
     open() {
       console.log('open');
+      this.open1 = true;
     }
   }
 }
