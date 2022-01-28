3654767edad8ae4f1f572c3ca69511009bdefda7
smw0807 <smw0807@gmail.com>
2022-01-28 23:50:47 +0900

    project_1 > 미들웨어에서 쿼리스트링 접근 로직 테스트 너무 단순하게 생각해서 만들기 쉬울 것 같았는데 생각해야할 문제가 생김...보류

--- project_1/web/middleware/router.js
+++ project_1/web/middleware/router.js
@@ -7,13 +7,35 @@
  * 
  * 보통은 토큰 유효성 체크로 사용하긴 했음
  */
-export default function ({store, route}) {
+export default function ({store, route, redirect, context}) {
+  const auth = ['/vue/authtest'];
   // console.log('middleware/router.js', store, route);
   // console.log('middleware/router.js : ', route);
   // console.log(route.query);
   // console.log(Object.keys(route.query).length === 0);
   // console.log(route.query.constructor === Object);
-  // if (Object.keys(route.query).length !== 0 && route.query.constructor === Object) {
-  //   //url에 query가 있을 경우.... 회사에서 권한 관련해서 필요해서 가능한지 테스트해본건데 가능한듯함.
+  
+  //2022-01-28 일단 다시 주석처리 다시 고민해보고 만들어야함
+  // console.log(route.path);
+  // if (auth.indexOf(route.path) !== -1) {
+  //   console.log(true);
+  //   if (Object.keys(route.query).length !== 0 && route.query.constructor === Object) {
+  //     //url에 query가 있을 경우.... 회사에서 권한 관련해서 필요해서 가능한지 테스트해본건데 가능한듯함.
+  //     console.log('yes query string : ', route);
+  //     console.log('yes query string : ', route.query);
+  //     console.log('yes query string : ', route.path);
+  //     if (Object.values(route.query)[0] === 'true') {
+  //       console.log('is_ok');
+  //       redirect(route.path); //이렇게 하면 결국 없음2에 걸려서 막힘;
+  //     } else {
+  //       console.warn('접근 권한이 없습니다.1');
+  //       alert('접근 권한이 없습니다.');
+  //       redirect('/');
+  //     }
+  //   } else {
+  //     console.warn('접근 권한이 없습니다.2');
+  //     alert('접근 권한이 없습니다.');
+  //     redirect('/');
+  //   }
   // }
 }
---
+++ project_1/web/pages/vue/authtest.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-card-title>권한 접근 테스트</v-card-title>
+    <v-card-text>
+      url로만 접근시 접근 권한 없다고 막고, 특정 쿼리스트링 값을 붙여서 접근하면 접근 가능하게..</br>
+      그리고 접속 시 url에 쿼리스트링은 안남게...
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
