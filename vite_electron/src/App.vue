4794dc734d1f184bf093a385469d75e28d0a100e
smw0807 <smw0807@gmail.com>
2022-02-12 18:43:17 +0900

    note commit

--- vite_electron/src/App.vue
+++ vite_electron/src/App.vue
@@ -5,8 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite + Electron" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite + Electron" />
+  </div>
 </template>
 
 <style>
