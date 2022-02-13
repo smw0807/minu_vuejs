bc686117cc14eb29205b49c83cef4243a7e5acad
smw0807 <smw0807@gmail.com>
2022-02-13 21:54:36 +0900

    vite에 quasar 적용 및 테스트 완료

--- vite_quasar/src/main.js
+++ vite_quasar/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+
+//퀘이사 추가
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {}
+})
+
+myApp.mount('#app');
--- vite_quasar/vite.config.js
+++ vite_quasar/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/assets/quasar/quasar-variables.sass'
+    })
+  ]
 })
