2b4ffc567dff31dce88744b9e42af531f294ab72
smw0807 <smw0807@gmail.com>
2022-02-14 19:18:06 +0900

    소스 정리

--- vite_quasar/src/main.js
+++ vite_quasar/src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
-
 //퀘이사 추가
 import { Quasar } from 'quasar'
-// Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
-// Import Quasar css
 import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
@@ -12,7 +9,6 @@ import App from './App.vue'
 import router from './router';
 
 const myApp = createApp(App);
-
 myApp.use(Quasar, {
   plugins: {}
 })
