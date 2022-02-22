33d5d7fd05b51ed3ba9b2b6a833c6db9cde0edde
smw0807 <smw0807@gmail.com>
2022-02-22 21:42:45 +0900

    vue3 따라해보기... 아직 이해를 못하겠음

--- vite_quasar/src/components/composition/props-test1/props1.vue
+++ vite_quasar/src/components/composition/props-test1/props1.vue
@@ -7,12 +7,19 @@ export default {
     console.log('props1: ', props);
   }
 }
+// export default {
+//   setup(props) {
+//     const { title } = toRefs(props);
+//     console.log(title);
+//     const title = toRef(props, 'title');
+//   }
+// }
 </script>
 
 <template>
   <div class="row">
     <div class="col">
-      {{title}}
+      <!-- {{title}} -->
     </div>
   </div>
 </template>
