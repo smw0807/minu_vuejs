3eb2b4279e3bdc10febddfd1aed72d3ecc2cc27c
smw0807 <smw0807@gmail.com>
2022-03-14 18:29:46 +0900

    vue3 > 테스트

--- vite_quasar/src/views/composition.vue
+++ vite_quasar/src/views/composition.vue
@@ -8,9 +8,25 @@ export default {
 </script>
 
 <template>
-  <ApiSetup/>
+  <div class="q-pa-md q-qutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-card-section>
+        <div class="text-h5">setup()</div>
+        <q-separator inset class="q-my-md" />
+        <ApiSetup/>
+      </q-card-section>
+    </q-card>
+    <q-card class="my-card q-mt-sm" flat bordered>
+      <q-card-section>
+        <div class="text-h5">setup()</div>
+        <q-separator inset class="q-my-md" />
+        <ApiSetup/>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
-<style scoped>
-
+<style lang="sass" scoped>
+.my-card
+  width: 100%
 </style>
