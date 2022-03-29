37a4e3d8c4ec053758a0b7538efb628a13562e52
smw0807 <smw0807@gmail.com>
2022-03-29 21:25:06 +0900

    github pages 테스트

--- vite_quasar/vite.config.js
+++ vite_quasar/vite.config.js
@@ -9,6 +9,7 @@ export default ({mode}) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
   return defineConfig({
+    base: '/',
     server: {
       host: '0.0.0.0',
       port: process.env.VITE_SERVER_PROT,
