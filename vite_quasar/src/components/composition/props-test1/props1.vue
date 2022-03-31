6de2b9b22137c0572ff3bb7242db86d0613078cb
smw0807 <smw0807@gmail.com>
2022-03-31 18:59:13 +0900

    vue3 props 관련 확인

--- vite_quasar/src/components/composition/props-test1/props1.vue
+++ vite_quasar/src/components/composition/props-test1/props1.vue
@@ -7,7 +7,7 @@ export default {
   setup(props) {
     //props 받는 3가지 방법...??
     // const { title } = ref(props); //undefined
-    const { title } = toRefs(props); //정상
+    let { title } = toRefs(props); //정상
     // const title = toRef(props, 'title'); //value 값만 찍힘
     // const title = ref(props.title); //정상
     // const title = ref(props, 'title'); //object key : value 다 찍힘
@@ -18,6 +18,7 @@ export default {
     // const title = toRefs(props, 'title'); //object key : value 다 찍힘
 
     console.log(title);
+    title = 'This is ' + props.title;
     return {
       title
     }
