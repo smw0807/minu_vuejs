4e5d22ef70b33257cd0f634b47585eb1e3862c47
smw0807 <smw0807@gmail.com>
2022-04-01 21:22:26 +0900

    vue3 > template refs 및 개인 테스트

---
+++ vite_quasar/src/components/composition/template-refs/refs-test.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    Is run now...{{run}}
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    let run = ref(false);
+    function isRun() {
+      run = ref(true);
+    }
+    return {
+      run, isRun,
+    }
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ vite_quasar/src/components/composition/template-refs/tpl-refs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div ref="root">This is a root element</div>
+    <br>
+    <refsTest ref="test1"/>
+  </div>
+</template>
+
+<script>
+import refsTest from './refs-test.vue';
+import { ref, onMounted } from 'vue';
+export default {
+  setup() {
+    const root = ref(null);
+    console.log(root);
+    
+    const test1 = ref(null);
+    onMounted(() => {
+      //DOM 요소는 초기 렌더링 후에 ref에 할당한다.
+      console.log(root.value);
+      console.log(test1); //vue2 처럼 ref를 이용해 해당 컴포넌트 안에 있는 함수 실행 가능한지 테스트해보기...
+    })
+    
+    return { 
+      root
+    }
+  },
+  components: {
+    refsTest
+  }
+}
+</script>
+
+<style>
+
+</style>
