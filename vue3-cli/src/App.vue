dc0f7e899efdac8473cd8b0e2d1335d27c146fc7
smw0807 <smw0807@gmail.com>
2022-04-04 20:29:41 +0900

    vue3-cli vue-cli에서 vue-drag-resize 테스트가 필요해서 설치함 (테스트는 완료)

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <dragResize/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import dragResize from './components/dragResize.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    dragResize
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-cli/src/components/dragResize.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <vue-drag-resize :isActive="true" :w="200" :h="200" v-on:resizing="resize" v-on:dragging="resize">
+      <h3>Hello World!</h3>
+      <p>{{ stat.top }} х {{ stat.left }} </p>
+      <p>{{ stat.width }} х {{ stat.height }}</p>
+    </vue-drag-resize>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import VueDragResize from 'vue-drag-resize';
+export default {
+  components: {
+    VueDragResize
+  },
+  setup() {
+    const stat = reactive({
+      width : 200,
+      height : 200,
+      top : 0,
+      left : 0,
+    })
+
+    const resize = (newRect) => {
+      stat.width = newRect.width;
+      stat.height = newRect.height;
+      stat.top = newRect.top;
+      stat.left = newRect.left;
+    }
+
+    return {
+      stat,
+      resize
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
