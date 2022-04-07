d486acc1639399e4953160d757f3304d4e441530
smw0807 <smw0807@gmail.com>
2022-04-07 21:14:12 +0900

    vue3 > reactive 방식 말곤 반응시킬 순 없는건가???

--- vite_quasar/src/components/composition/template-refs/refs-test.vue
+++ vite_quasar/src/components/composition/template-refs/refs-test.vue
@@ -1,23 +1,29 @@
 <template lang="">
   <div>
-    Is run now...{{run}}
+    Is run now...{{data.run}}
   </div>
 </template>
 <script>
-import { ref , toRefs} from 'vue';
+import { ref , toRefs, reactive} from 'vue';
 export default {
   setup() {
-    let run = ref(false);
+    const data = reactive({
+      run: false
+    })
+    let run = false;
+    console.log(run);
     let a = ref('aaaa');
     let b = ref('bbbb');
     let c = ref(1234);
     const isRun = (v) => {
       console.log('isRun');
       console.log('부모 컴포넌트에게 받은 값: ', v);
-      run = ref(true);
+      data.run = true;
+      console.log(run);
     }
     // console.log('run : ' , run);
     return {
+      data,
       run, 
       a,
       b,
