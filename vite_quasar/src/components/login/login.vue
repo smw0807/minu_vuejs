5e6604dd331703a1258ceef12edcd48b0ce276b8
smw0807 <smw0807@gmail.com>
2022-04-10 21:24:53 +0900

    vite+vue3 로그인 기능 만들어보기...2 로그인 다이얼로그 레이아웃은 완료됨 이제 토큰 체크 로직 넣어서 없으면 열리게할 예정

--- vite_quasar/src/components/login/login.vue
+++ vite_quasar/src/components/login/login.vue
@@ -1,38 +1,37 @@
 <template lang="">
   <div class="q-pa-md q-gutter-sm">
-    <q-btn label="Click me" color="primary" @click="persistent = true" />
+    <q-btn label="Click me" color="primary" @click="open()" />
 
-    <q-dialog v-model="persistent" persistent full-width transition-show="scale" transition-hide="scale">
+    <q-dialog v-model="isLoginPop" persistent transition-show="scale" transition-hide="scale">
       <q-card class="bg-teal text-white">
         <q-card-section>
-          <div class="text-h6">로그인</div>
+          <div class="text-h6">Please login...</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <q-page class=" row justify-center items-center">
-            <div class="column">
-              <div class="row">
-                <q-card square bordered class="q-pa-lg shadow-1">
-                  <q-card-section>
-                    <q-form class="q-gutter-md">
-                      <q-input square filled clearable v-model="email" type="email" label="email" />
-                      <q-input square filled clearable v-model="password" type="password" label="password" />
-                    </q-form>
-                  </q-card-section>
-                  <q-card-actions class="q-px-md">
-                    <q-btn unelevated color="light-green-7" size="lg" class="full-width" label="Login" />
-                  </q-card-actions>
-                  <q-card-section class="text-center q-pa-none">
-                    <p class="text-grey-6">Not reigistered? Created an Account</p>
-                  </q-card-section>
-                </q-card>
-              </div>
+          <div class="column">
+            <div class="row justify-center items-center">
+              <q-card square bordered class="q-pa-lg shadow-1">
+                <q-card-section>
+                  <q-form class="q-gutter-md">
+                    <q-input square filled clearable v-model="email" type="email" label="email" />
+                    <q-input square filled clearable v-model="password" type="password" label="password" />
+                  </q-form>
+                </q-card-section>
+                <q-card-actions class="q-px-md">
+                  <q-btn unelevated color="light-green-7" size="lg" class="full-width" label="Login" @click="login" />
+                </q-card-actions>
+                <!-- <q-card-section class="text-center q-pa-none">
+                  <p class="text-grey-6">Not reigistered? Created an Account</p>
+                </q-card-section> -->
+              </q-card>
             </div>
-          </q-page>
+          </div>
         </q-card-section>
 
         <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="OK" v-close-popup />
+          <q-btn flat label="Close" v-close-popup />
+          <!-- <q-btn flat label="Close" v-close-popup /> -->
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -42,10 +41,27 @@
 import { ref } from 'vue';
 export default {
   setup() {
+    let isLoginPop = ref(false);
+    const email = ref('admin');
+    const password = ref('aaaa');
+
+    //@@ 로그인 팝업 오픈
+    const open = () => {
+      isLoginPop.value = true;
+    }
+
+    //@@ 로그인 처리
+    const login = () => {
+      // isLoginPop = ref(false); // <- 이건 왜 안닫히는 걸까?? 차이점을 이해를 못하겠다.
+      isLoginPop.value = false;
+    }
+
     return {
-      persistent: ref(false),
-      email: ref('admin'),
-      password: ref('aaaa'),
+      isLoginPop,
+      email,
+      password,
+      open, //팝업 오픈
+      login
     }
   }
 }
