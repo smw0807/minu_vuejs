0ca04704c6e34b97ea623962b42ec73aff92a784
smw0807 <smw0807@gmail.com>
2022-04-18 20:48:55 +0900

    vite+vue3 jwt...axios 수정중 토큰 없는 상태로 요청에 대한 로직 작업중... 머리가 아파서 일단포기

--- vite_quasar/src/plugins/interceptor.js
+++ vite_quasar/src/plugins/interceptor.js
@@ -4,10 +4,13 @@ const { cookies } = useCookies();
 
 const setup = (store) => {
   instance.interceptors.request.use(
-    (config) => {
+    async (config) => {
       console.log('axios.js request : ' , config);
       //token 체크 로직?
       if(import.meta.env.VITE_IS_LOGIN === 'Y') {
+        // if (cookies.get('accessToken') === null) {
+        //   await store.dispatch('auth/refresh')
+        // }
         config.headers['x-access-token'] = cookies.get('accessToken');
         config.headers['x-refresh-token'] = cookies.get('refreshToken');
         return config;
@@ -27,6 +30,20 @@ const setup = (store) => {
     },
     (error) => {
       console.error('axios.js reqponse error : ', error);
+      if(import.meta.env.VITE_IS_LOGIN === 'Y') {
+        const errorAPI = error.response.config;
+        console.log('errorAPI : ', errorAPI);
+        // if (cookies.get('refreshToken') === null) {
+        //   console.error('요청 중 refreshToken 토큰 없음');
+        //   store.commit('auth/needLogin', true);
+        //   return false;
+        // }
+        // if (cookies.get('accessToken') === null) {
+        //   console.log('요청 중 accessToken 없음');
+        //   await store.dispatch('auth/refreshToken');
+        //   return config
+        // }
+      }
       return Promise.reject(error);
     }
   )
