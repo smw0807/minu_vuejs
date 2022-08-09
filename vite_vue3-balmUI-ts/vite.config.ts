465a6a1122ec59d8a4f8d911ccebb47953c51396
smw0807 <smw0807@gmail.com>
2022-08-09 11:51:29 +0900

    balmUI 적용 중복 소스 제거

--- vite_vue3-balmUI-ts/vite.config.ts
+++ vite_vue3-balmUI-ts/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +9,4 @@ export default defineConfig({
     strictPort: true, //true로 하면 설정 포트가 이미 다른 곳에서 사용중일 때 다음 포트로 시도하지 않음
   },
   plugins: [vue()],
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
-      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
-      'balm-ui-css': 'balm-ui/dist/balm-ui.css'
-    }
-  }
 })
