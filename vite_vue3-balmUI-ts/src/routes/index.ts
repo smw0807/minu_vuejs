a91e1145d832c9c2f75c53e0d99336406b124b74
smw0807 <smw0807@gmail.com>
2022-08-09 11:52:26 +0900

    vue-router 에서 제공하는 scrollBehavior 적용 이런게 있는 줄도 몰랐네

--- vite_vue3-balmUI-ts/src/routes/index.ts
+++ vite_vue3-balmUI-ts/src/routes/index.ts
@@ -6,6 +6,12 @@ export const router = createRouter({
   history: createWebHistory(),
   routes,
   strict: true,
+  scrollBehavior(to, from, savedPosition) {
+    return { // 페이지 이동시 맨 위로 부드럽게 이동
+      top: 0,
+      behavior: 'smooth',
+    }
+  }
 })
 
 //router 적용
