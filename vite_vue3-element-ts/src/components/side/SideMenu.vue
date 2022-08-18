892e37fa5707c90ef321cbb9008ce2c69a18b1af
smw0807 <smw0807@gmail.com>
2022-08-18 18:14:37 +0900

    불필요 소스 제거

--- vite_vue3-element-ts/src/components/side/SideMenu.vue
+++ vite_vue3-element-ts/src/components/side/SideMenu.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import type { Ref } from 'vue';
 import { useRoute } from 'vue-router';
 const route = useRoute();
 const path = computed(() => route.path);
-//======== menu =====================
+
 import menusArr from '@/routes/routes.config';
 const menus: Array<object> = menusArr.filter(() => true);
 </script>
