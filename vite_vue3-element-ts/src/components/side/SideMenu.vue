33cea3375ffa1b7f20ae8c8942a5b23a1920bd58
smw0807 <smw0807@gmail.com>
2022-08-18 18:13:43 +0900

    현재 메뉴 active 제대로 안되던거 수정

--- vite_vue3-element-ts/src/components/side/SideMenu.vue
+++ vite_vue3-element-ts/src/components/side/SideMenu.vue
@@ -1,33 +1,20 @@
 <script setup lang="ts">
-import { ref,  onMounted, computed } from 'vue';
+import { computed } from 'vue';
 import type { Ref } from 'vue';
 import { useRoute } from 'vue-router';
 const route = useRoute();
-
+const path = computed(() => route.path);
 //======== menu =====================
 import menusArr from '@/routes/routes.config';
 const menus: Array<object> = menusArr.filter(() => true);
-const menuSelect = (v: string): void => {
-  defaultAction.value = v;
-}
-
-let test = computed(() => route.path);
-console.log(test);
-let defaultAction: Ref<string> = ref(route.path);
-onMounted(() => {
-  console.log('route.path : ', route.path);
-  defaultAction.value = route.path;
-  console.log(defaultAction);
-});
 </script>
 <template>
   <el-row>
     <el-col>
       <el-menu
-          :default-active="defaultAction"
+          :default-active="path"
           class="el-menu-vertical-demo"
           :router="true"
-          @select="menuSelect"
           >
           <el-menu-item v-for="(menu, idx) in menus" :index="menu.path" :key="idx">
             <span>{{menu.name}}</span>
