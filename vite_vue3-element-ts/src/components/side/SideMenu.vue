37a3c8fda1d633e121ff33c6b68b1e03de6a0b1e
smw0807 <smw0807@gmail.com>
2022-08-18 17:25:22 +0900

    사이드 메뉴

--- vite_vue3-element-ts/src/components/side/SideMenu.vue
+++ vite_vue3-element-ts/src/components/side/SideMenu.vue
@@ -1,51 +1,36 @@
 <script setup lang="ts">
-import {
-  HomeFilled
-} from '@element-plus/icons-vue'
-// const handleOpen = (key: string, keyPath: string[]): void => {
-  // console.log(key, keyPath)
-// }
-// const handleClose = (key: string, keyPath: string[]): void => {
-  // console.log(key, keyPath)
-// }
+import { ref,  onMounted, computed } from 'vue';
+import type { Ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+//======== menu =====================
+import menusArr from '@/routes/routes.config';
+const menus: Array<object> = menusArr.filter(() => true);
+const menuSelect = (v: string): void => {
+  defaultAction.value = v;
+}
+
+let test = computed(() => route.path);
+console.log(test);
+let defaultAction: Ref<string> = ref(route.path);
+onMounted(() => {
+  console.log('route.path : ', route.path);
+  defaultAction.value = route.path;
+  console.log(defaultAction);
+});
 </script>
 <template>
   <el-row>
     <el-col>
       <el-menu
-          default-active="2"
+          :default-active="defaultAction"
           class="el-menu-vertical-demo"
-        >
-          <!-- @open="handleOpen"
-          @close="handleClose" -->
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><HomeFilled /></el-icon>
-              <span>Navigator One</span>
-            </template>
-            <el-menu-item-group title="Group One">
-              <el-menu-item index="1-1">item one</el-menu-item>
-              <el-menu-item index="1-2">item one</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>item four</template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="2">
-            <el-icon></el-icon>
-            <span>Navigator Two</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <el-icon></el-icon>
-            <span>Navigator Three</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <el-icon></el-icon>
-            <span>Navigator Four</span>
+          :router="true"
+          @select="menuSelect"
+          >
+          <el-menu-item v-for="(menu, idx) in menus" :index="menu.path" :key="idx">
+            <span>{{menu.name}}</span>
           </el-menu-item>
         </el-menu>
     </el-col>
---
+++ vite_vue3-element-ts/src/views/test/test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    <el-row align="middle" justify="center">
+      <el-col>
+        gdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgd
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+.main {
+
+}
+</style>
