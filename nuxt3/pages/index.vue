461546345234afda4e03a75531f0739e11d2568a
Minu Song <smw0807@gmail.com>
2023-03-26 18:26:36 +0900

    vueuse 설치 및 테스트

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import {useImage } from '@vueuse/core';
+
+const avatarUrl = 'http://place.dog/300/200';
+const { isLoading } = useImage({src: avatarUrl})
 //컴포져블 테스트
 const foo = useFoo();
 console.log(foo);
@@ -6,5 +10,7 @@ console.log(foo);
 <template>
   <test/>
   <BaseInput/>
+  <span v-if="isLoading">Loading</span>
+  <img v-else :src="avatarUrl">
   <NuxtWelcome />
 </template>
