6914749796ecb2faf365011a2caccba557576d16
Minu Song <smw0807@gmail.com>
2023-05-07 13:54:39 +0900

    vite.config.ts ssr, pages 적용

--- vite-vue3-ssr/vite.config.ts
+++ vite-vue3-ssr/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import ssr from 'vite-plugin-ssr/plugin';
+import pages from 'vite-plugin-pages';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), ssr(), pages()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: resolve(__dirname, 'src'),
+      },
+    ],
+  },
+});
