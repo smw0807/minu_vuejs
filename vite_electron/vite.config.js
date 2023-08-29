5fc43e0dad10ce97a313866570fbafb0d89e8e94
Minu Song <smw0807@gmail.com>
2023-08-29 21:20:33 +0900

    vite_electron : 레파지토리 변경 minu_vuejs -> vue3 레파지토리로 위치 변경

--- vite_electron/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path');
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? path.resolve(__dirname, 'dist') : "",
-  plugins: [vue()]
-})
